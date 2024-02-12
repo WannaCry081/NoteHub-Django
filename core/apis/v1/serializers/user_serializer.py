e1aef6bb2d4334209c6fd3cfd94ce03512d10efe
Lirae Que Data <liraedata59@gmail.com>
2024-02-12 23:32:12 +0800

    Add validate method in user_serializer.py

--- core/apis/v1/serializers/user_serializer.py
+++ core/apis/v1/serializers/user_serializer.py
@@ -1,3 +1,4 @@
+import bleach
 from rest_framework import serializers
 from core.models import User
 
@@ -16,5 +17,25 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "password" : {"write_only" : True, "style" : {"input_type" : "password"}},
         }
+    
+    def validate(self, attrs):
+
+        if "username" in attrs:
+            attrs["username"] = bleach.clean(attrs["username"])
+        if "email" in attrs:
+            attrs["email"] = bleach.clean(attrs["email"])
+        if "first_name" in attrs:
+            attrs["first_name"] = bleach.clean(attrs["first_name"])
+        if "middle_name" in attrs:
+            attrs["middle_name"] = bleach.clean(attrs["middle_name"])
+        if "last_name" in attrs:
+            attrs["last_name"] = bleach.clean(attrs["last_name"])
+            
+        if attrs["password"] != attrs["re_password"]:
+            raise serializers.ValidationError("Invalid credentails. Please try again.")
+            
+        attrs.pop("re_password")
+        return attrs
+    
     
     
