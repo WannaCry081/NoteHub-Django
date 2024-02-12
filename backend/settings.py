2d0ccfffb2ab9ca495f78b6927bd4ef2c63a87cc
Lirae Que Data <liraedata59@gmail.com>
2024-02-12 22:14:57 +0800

    Add permissions in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -135,4 +135,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES' : [
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
+    'DEFAULT_PERMISSIONS_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticated'
+    ]
 }
