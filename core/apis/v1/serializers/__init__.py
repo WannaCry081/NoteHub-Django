e5e5e1747a9c0fd6d9eb7d833642c99231fa6d57
Lirae Que Data <liraedata59@gmail.com>
2024-02-12 23:33:09 +0800

    Add exports serializer __init__.py

---
+++ core/apis/v1/serializers/__init__.py
@@ -0,0 +1,8 @@
+from core.apis.v1.serializers.user_serializer import UserSerializer
+from core.apis.v1.serializers.login_serializer import LoginSerializer
+
+
+__all__ = [
+    "UserSerializer",
+    "LoginSerializer"
+]
