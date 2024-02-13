74f6e17d953673cb9e75da1d5387ca5cad603992
Lirae Que Data <liraedata59@gmail.com>
2024-02-13 18:55:50 +0800

    Update fieldsets in user_admin.py

--- core/admins/user_admin.py
+++ core/admins/user_admin.py
@@ -6,11 +6,11 @@ class UserAdmin(BaseUserAdmin):
     def get_fieldsets(self, request, obj = None):
         
         fieldsets = super().get_fieldsets(request, obj)
-        
+
         if obj:
             fieldsets = (
                 (None, {"fields" : ("username", "email", "password")}),
-                ("Personal info", {"fields" : ("first_name", "middle_name", "last_name")}),
+                ("Personal info", {"fields" : ("first_name", "middle_name", "last_name", "teams")}),
             ) + fieldsets[2:]
             
         return fieldsets
