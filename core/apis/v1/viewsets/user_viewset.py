15c68cdbf43390237e69d675f590b94c5abc7977
Lirae Que Data <liraedata59@gmail.com>
2024-02-13 17:14:51 +0800

    Add list method in user_viewset.py

--- core/apis/v1/viewsets/user_viewset.py
+++ core/apis/v1/viewsets/user_viewset.py
@@ -1,13 +1,28 @@
 from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from core.models import User
 from core.apis.v1.serializers import UserSerializer
 
+
 class UserViewSet(viewsets.GenericViewSet, 
-                  mixins.ListModelMixin):
+                  mixins.ListModelMixin,
+                  mixins.DestroyModelMixin):
     
     queryset = User.objects.all()
     serializer_class = UserSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+    
+    
+    def list(self, request, *args, **kwargs):
+        
+        try:
+            queryset = self.filter_queryset(request.user)
+            serializer = self.get_serializer(queryset)
+            
+        except:
+            return Response({"detail" : "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
