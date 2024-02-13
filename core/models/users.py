928d7671126e83c3340dc1bb3864a8ede80a6188
Lirae Que Data <liraedata59@gmail.com>
2024-02-13 18:54:52 +0800

    Update user models in user.py

--- core/models/users.py
+++ core/models/users.py
@@ -4,11 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     
+    profile = models.URLField(blank = True)
     username = models.CharField(max_length = 100, unique = True)
     first_name = models.CharField(max_length = 40)
     middle_name = models.CharField(max_length = 40)
     last_name = models.CharField(max_length = 40)
     email = models.EmailField(unique = True)
+    
+    teams = models.ManyToManyField("Team", related_name="team_user", blank = True)
     
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
