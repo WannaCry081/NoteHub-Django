7b0a3b3fe71796a7b941d051917f87d2cfcc0c2d
Lirae Que Data <liraedata59@gmail.com>
2024-02-18 14:30:44 +0800

    Create TeamViewSet in team_viewset.py

---
+++ core/apis/v1/viewsets/team_viewset.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from core.models import Team
+from core.apis.v1.serializers import TeamSerializer
+
+
+class TeamViewSet(viewsets.GenericViewSet,
+                  mixins.ListModelMixin, 
+                  mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin):
+    
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
