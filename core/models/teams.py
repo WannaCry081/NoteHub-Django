13eefaa1e889df6a2a704a9ff810fe99525d07bf
Lirae Que Data <liraedata59@gmail.com>
2024-02-18 13:24:52 +0800

    Update team models in teams.py

--- core/models/teams.py
+++ core/models/teams.py
@@ -3,11 +3,15 @@ from django.db import models
 
 class Team(models.Model):
     
-    profile = models.URLField(blank = True)
+    profile = models.URLField(blank = True, null = True)
     name = models.CharField(max_length = 20, unique = True)
     description = models.TextField(blank = True)
     
     members = models.ManyToManyField("User", related_name="user_team", blank = True)
+    
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
     
     def __str__(self):
         return self.name
