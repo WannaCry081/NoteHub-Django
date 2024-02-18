29f7129f70b476f43382b824f9bad2c65ed2e57e
Lirae Que Data <liraedata59@gmail.com>
2024-02-18 14:31:53 +0800

    Add export team serializer export

--- core/apis/v1/serializers/__init__.py
+++ core/apis/v1/serializers/__init__.py
@@ -1,8 +1,10 @@
 from core.apis.v1.serializers.user_serializer import UserSerializer
 from core.apis.v1.serializers.login_serializer import LoginSerializer
+from core.apis.v1.serializers.team_serializer import TeamSerializer
 
 
 __all__ = [
     "UserSerializer",
-    "LoginSerializer"
+    "LoginSerializer",
+    "TeamSerializer"
 ]
