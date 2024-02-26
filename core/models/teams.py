665699eae07d37bddfa814887ceb774e9582a7ff
Lirae Que Data <liraedata59@gmail.com>
2024-02-26 11:44:34 +0800

    Update code field property in teams.py

--- core/models/teams.py
+++ core/models/teams.py
@@ -4,7 +4,7 @@ from core.utils import code_generator
 class Team(models.Model):
     
     profile = models.URLField(blank = True, null = True)
-    code = models.CharField(max_length = 8, default=code_generator)
+    code = models.CharField(max_length = 8, default=code_generator, blank = True)
     name = models.CharField(max_length = 20, unique = True)
     description = models.TextField(blank = True)
     
