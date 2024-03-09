ea37e68b02eb9430e4a2adeec4ebff0fe6fbc6ef
Lirae Que Data <liraedata59@gmail.com>
2024-03-09 23:01:01 +0800

    Add partial_update method in team_viewset.py

--- core/apis/v1/viewsets/team_viewset.py
+++ core/apis/v1/viewsets/team_viewset.py
@@ -50,5 +50,9 @@ class TeamViewSet(viewsets.GenericViewSet,
         return super().update(request, *args, **kwargs)
     
     
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+    
+    
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
