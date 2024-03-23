f5cc0887de7021760a23486daa398f75060cfc46
Lirae Que Data <liraedata59@gmail.com>
2024-03-24 00:19:34 +0800

    Add noteviewset in __init__.py

--- api/core/v1/viewsets/__init__.py
+++ api/core/v1/viewsets/__init__.py
@@ -2,11 +2,13 @@ from api.core.v1.viewsets.register_viewset import RegisterViewSet
 from api.core.v1.viewsets.login_viewset import LoginViewSet
 from api.core.v1.viewsets.user_viewset import UserViewSet
 from api.core.v1.viewsets.team_viewset import TeamViewSet
+from api.core.v1.viewsets.note_viewset import NoteViewSet
 
 
 __all__ = [
     "RegisterViewSet",
     "LoginViewSet",
     "UserViewSet",
-    "TeamViewSet"
+    "TeamViewSet",
+    "NoteViewSet"
 ]
