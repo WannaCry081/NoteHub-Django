1b72453ce1bfae21ced2d77fc20fb61cb076ec46
Lirae Que Data <liraedata59@gmail.com>
2024-03-24 00:13:34 +0800

    Add note serializer in note_serializer.py

---
+++ api/core/v1/serializers/note_serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from api.models import Note
+
+
+class NoteSerializer(serializers):
+    
+    
+    class Meta:
+        
+        model = Note
+        fields = ["title", "body", "team"]
+        extra_kwargs = {
+            "team" : {"read_only" : True}
+        }
