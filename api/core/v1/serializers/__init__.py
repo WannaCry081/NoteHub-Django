2110fc33154a323529e34346afc0d4b3135a50eb
Lirae Que Data <liraedata59@gmail.com>
2024-03-24 00:14:04 +0800

    Add note serializer in __init__.py

--- api/core/v1/serializers/__init__.py
+++ api/core/v1/serializers/__init__.py
@@ -2,6 +2,7 @@ from api.core.v1.serializers.user_serializer import UserSerializer
 from api.core.v1.serializers.login_serializer import LoginSerializer
 from api.core.v1.serializers.team_serializer import TeamSerializer
 from api.core.v1.serializers.join_team_serializer import JoinTeamSerializer
+from api.core.v1.serializers.note_serializer import NoteSerializer
 
 
 __all__ = [
@@ -9,4 +10,5 @@ __all__ = [
     "LoginSerializer",
     "TeamSerializer",
     "JoinTeamSerializer",
+    "NoteSerializer"
 ]
