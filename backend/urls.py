e8a8b2c302ebcf900ac094902a5d6e2adfb9608f
Lirae Que Data <liraedata59@gmail.com>
2024-03-31 00:13:18 +0800

    Add drf_yasg in urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -16,9 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = "NoteHub - API",
+        default_version = "v1",
+        description = "A Note App Application Programming Interface (API) that provides a user-team experience.",
+    ),
+    public = True,
+    permission_classes = (AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('swagger<format>/', schema_view.without_ui(), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger'), name='schema-redoc'),
+    path('redoc/', schema_view.with_ui('redoc'), name='schema-redoc')
 ]
