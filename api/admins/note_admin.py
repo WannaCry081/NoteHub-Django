0d9dd6c3f4c51ecdf90a2dd0737f15086c9bacf7
Lirae Que Data <liraedata59@gmail.com>
2024-03-30 17:23:12 +0800

    Add short_body method in note_admin.py

--- api/admins/note_admin.py
+++ api/admins/note_admin.py
@@ -4,5 +4,15 @@ from api.models import Note
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "body", "created_at", "updated_at")
+    list_display = ("id", "title", "short_body", "team", "created_at", "updated_at")
     search_fields = ("title", "body")
+    
+    def short_body(self, obj):
+        words_limit = 10
+        body = obj.body
+        if len(body.split()) > words_limit:
+            return ' '.join(body.split()[:words_limit]) + '...'
+        else:
+            return body
+    
+    short_body.short_description = 'Body'
