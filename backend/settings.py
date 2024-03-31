b989cd6f2d71bd2863c5be54e15a19fe50f7b578
Lirae Que Data <liraedata59@gmail.com>
2024-03-31 13:31:00 +0800

    Add pagination in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -153,6 +153,8 @@ REST_FRAMEWORK = {
         'anon' : '50/day',
         'user' : '1000/day'
     },
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE' : 10
 }
 
 SIMPLE_JWT = {
