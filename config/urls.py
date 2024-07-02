a01a61a80c48f6bfd24c895ea65158b26d475297
Lirae Que Data <liraedata59@gmail.com>
2024-07-02 15:33:40 +0800

    Update urlpatterns environments setup

--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+ENVIRONMENT : str = str(os.environ.get("DJANGO_ENV")).lower()
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -34,9 +40,16 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('swagger<format>/', schema_view.without_ui(), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger'), name='schema-redoc'),
-    path('redoc/', schema_view.with_ui('redoc'), name='schema-redoc')
+    path('api/', include([
+        path('v1/', include('api.v1.urls'))
+    ])),
+    
 ]
+
+if ENVIRONMENT == "development":
+    urlpatterns += [
+        path('admin/', admin.site.urls),
+        path('swagger<format>/', schema_view.without_ui(), name='schema-json'),
+        path('swagger/', schema_view.with_ui('swagger'), name='schema-redoc'),
+        path('redoc/', schema_view.with_ui('redoc'), name='schema-redoc')
+    ]
